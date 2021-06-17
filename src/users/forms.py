78c5b63be2c6e604161cdc69aa8a749fa94227b7
Imangali <Imangali@gmail.com>
2021-06-17 21:19:40 +0600

    Added teacher signup with tests, login

--- src/users/forms.py
+++ src/users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.db import transaction
 
-from users.models import Student, User
+from users.models import Teacher, Student, User
 
 class StudentSignUpForm(UserCreationForm):
 
@@ -17,4 +17,20 @@ class StudentSignUpForm(UserCreationForm):
         user.is_student = True
         user.save()
         student = Student.objects.create(user=user)
+        return user
+
+
+class TeacherSignUpForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("email",)
+        field_classes = {'email': UsernameField}
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        user.save()
+        teacher = Teacher.objects.create(user=user)
         return user
--- src/users/tests/test_models.py
+++ src/users/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import User, Student
+from ..models import User, Student, Teacher
 
 class StudentModelTest(TestCase):
 
@@ -22,3 +22,25 @@ class StudentModelTest(TestCase):
         
         self.assertTrue(user.is_student)
         self.assertEqual(user, student.user)
+
+class TeacherModelTest(TestCase):
+
+    def setUp(self):
+        email = 'Imangali02@gmail.com'
+        password = 'Pu$$Y123123' 
+        user = User.objects.create_user(email = email, password = password)
+        user.is_teacher = True
+        user.save()
+        teacher = Teacher.objects.create(user=user)
+    
+    def test_user_email(self):
+        user = User.objects.get(id=1)
+
+        self.assertEqual('Imangali02@gmail.com', user.email)
+    
+    def test_teacher_user_relation(self):
+        user = User.objects.get(id=1)
+        teacher = Teacher.objects.get(user_id=1)
+        
+        self.assertTrue(user.is_teacher)
+        self.assertEqual(user, teacher.user)
